59f6222a8092f527f2ac3604b727b20be13b919a
austinv11 <austinv11@gmail.com>
2014-11-01 16:11:06 -0400

    Fixing more bugs, added getOreDicEntries()

--- src/main/java/com/austinv11/peripheralsplusplus/turtles/peripherals/PeripheralBarrel.java
+++ src/main/java/com/austinv11/peripheralsplusplus/turtles/peripherals/PeripheralBarrel.java
@@ -7,12 +7,13 @@ import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.turtle.ITurtleAccess;
 import dan200.computercraft.api.turtle.TurtleSide;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
+import net.minecraftforge.oredict.OreDictionary;
+
+import java.util.HashMap;
 
 public class PeripheralBarrel implements IPeripheral {
 
@@ -58,90 +59,96 @@ public class PeripheralBarrel implements IPeripheral {
 
 	@Override
 	public String[] getMethodNames() {
-		return new String[]{"get", "put", "getUnlocalizedItemName", "getLocalizedItemName", "getItemID", "getAmount"};
+		return new String[]{"get", "put", "getUnlocalizedName", "getLocalizedName", "getItemID", "getAmount", "getOreDictEntries"};
 	}
 
 	@Override
 	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException {
 		if (!Config.enableBarrelTurtle)
 			throw new LuaException("Barrel Turtles have been disabled");
-		try {
-			if (method == 0) {
-				int amount = STACK_SIZE;
-				if (arguments.length > 0) {
-					if (!(arguments[0] instanceof Double))
-						throw new LuaException("Bad argument #1 (expected number)");
-					double temp = (Double) arguments[0];
-					amount = (int) temp;
-				}
-				if (CURRENT_USAGE < amount)
-					amount = CURRENT_USAGE;
-				if (ITEM_TYPE_STORED == null)
-					return new Object[]{0};
-				ItemStack slot = turtle.getInventory().getStackInSlot(turtle.getSelectedSlot());
-				Item itemStored = ITEM_TYPE_STORED;
-				if (slot != null) {
-					if (!slot.isItemEqual(new ItemStack(itemStored)))
-						throw new LuaException("Item mismatch");
-					amount = amount - slot.stackSize;
-				}
-				ItemStack stack = new ItemStack(itemStored);
-				stack.stackSize = amount + slot.stackSize;
-				CURRENT_USAGE = CURRENT_USAGE - amount;
-				checkUsageStats();
-				turtle.getInventory().setInventorySlotContents(turtle.getSelectedSlot(), stack);
-				changed = true;
-				return new Object[]{amount};
-			} else if (method == 1) {
-				int amount = 64;
-				if (arguments.length > 0) {
-					if (!(arguments[0] instanceof Double))
-						throw new LuaException("Bad argument #1 (expected number)");
-					double temp = (Double) arguments[0];
-					amount = (int) temp;
-				}
-				ItemStack items = turtle.getInventory().getStackInSlot(turtle.getSelectedSlot());
-				if (amount > items.stackSize)
-					amount = items.stackSize;
-				if (amount > (MAX_SIZE - CURRENT_USAGE))
-					amount = MAX_SIZE - CURRENT_USAGE;
-				if (ITEM_TYPE_STORED != null) {
-					ItemStack temp = new ItemStack(ITEM_TYPE_STORED);
-					if (!temp.isItemEqual(items))
-						throw new LuaException("Item mismatch");
-				} else {
-					Item type = items.getItem();
-					ITEM_TYPE_STORED = type;
-					STACK_SIZE = type.getItemStackLimit(items);
-					MAX_SIZE = 64 * STACK_SIZE;
-				}
-				CURRENT_USAGE = CURRENT_USAGE + amount;
-				ItemStack newStack = new ItemStack(items.getItem());
-				if (items.stackSize - amount <= 0) {
-					newStack = null;
-				} else {
-					newStack.stackSize = items.stackSize - amount;
-				}
-				turtle.getInventory().setInventorySlotContents(turtle.getSelectedSlot(), newStack);
-				changed = true;
-				return new Object[]{amount};
-			} else if (method == 2) {
-				if (ITEM_TYPE_STORED != null)
-					return new Object[]{getUnwrappedUnlocalizedName(ITEM_TYPE_STORED.getUnlocalizedName())};
-			} else if (method == 3) {
-				if (ITEM_TYPE_STORED != null)
-					return new Object[]{StatCollector.translateToLocal(ITEM_TYPE_STORED.getUnlocalizedName())};
-			} else if (method == 4) {
-				if (ITEM_TYPE_STORED != null)
-					return new Object[]{Item.getIdFromItem(ITEM_TYPE_STORED)};
-			} else if (method == 5) {
-				int amount = 0;
-				if (ITEM_TYPE_STORED != null)
-					amount = CURRENT_USAGE;
-				return new Object[]{amount};
+		if (method == 0) {
+			int amount = STACK_SIZE;
+			if (arguments.length > 0) {
+				if (!(arguments[0] instanceof Double))
+					throw new LuaException("Bad argument #1 (expected number)");
+				double temp = (Double) arguments[0];
+				amount = (int) temp;
 			}
-		}catch (Exception e) {
-			e.printStackTrace();
+			if (CURRENT_USAGE < amount)
+				amount = CURRENT_USAGE;
+			if (ITEM_TYPE_STORED == null)
+				return new Object[]{0};
+			ItemStack slot = turtle.getInventory().getStackInSlot(turtle.getSelectedSlot());
+			Item itemStored = ITEM_TYPE_STORED;
+			int stackCount = amount;
+			if (slot != null) {
+				if (!slot.isItemEqual(new ItemStack(itemStored)))
+					throw new LuaException("Item mismatch");
+				if (amount + slot.stackSize > STACK_SIZE)
+					amount = STACK_SIZE - slot.stackSize;
+				stackCount = amount + slot.stackSize;
+			}
+			ItemStack stack = new ItemStack(itemStored);
+			stack.stackSize = stackCount;
+			CURRENT_USAGE = CURRENT_USAGE - amount;
+			checkUsageStats();
+			turtle.getInventory().setInventorySlotContents(turtle.getSelectedSlot(), stack.copy());
+			changed = true;
+			return new Object[]{amount};
+		} else if (method == 1) {
+			int amount = 64;
+			if (arguments.length > 0) {
+				if (!(arguments[0] instanceof Double))
+					throw new LuaException("Bad argument #1 (expected number)");
+				double temp = (Double) arguments[0];
+				amount = (int) temp;
+			}
+			ItemStack items = turtle.getInventory().getStackInSlot(turtle.getSelectedSlot()).copy();
+			if (amount > items.stackSize)
+				amount = items.stackSize;
+			if (amount > (MAX_SIZE - CURRENT_USAGE))
+				amount = MAX_SIZE - CURRENT_USAGE;
+			if (ITEM_TYPE_STORED != null) {
+				ItemStack temp = new ItemStack(ITEM_TYPE_STORED);
+				if (!temp.isItemEqual(items))
+					throw new LuaException("Item mismatch");
+			} else {
+				Item type = items.getItem();
+				ITEM_TYPE_STORED = type;
+				STACK_SIZE = type.getItemStackLimit(items);
+				MAX_SIZE = 64 * STACK_SIZE;
+			}
+			CURRENT_USAGE = CURRENT_USAGE + amount;
+			ItemStack newStack = new ItemStack(items.getItem());
+			if (items.stackSize - amount <= 0) {
+				newStack = null;
+			} else {
+				newStack.stackSize = items.stackSize - amount;
+			}
+			turtle.getInventory().setInventorySlotContents(turtle.getSelectedSlot(), newStack);
+			changed = true;
+			return new Object[]{amount};
+		} else if (method == 2) {
+			if (ITEM_TYPE_STORED != null)
+				return new Object[]{getUnwrappedUnlocalizedName(ITEM_TYPE_STORED.getUnlocalizedName(new ItemStack(ITEM_TYPE_STORED)))};
+		} else if (method == 3) {
+			if (ITEM_TYPE_STORED != null)
+				return new Object[]{StatCollector.translateToLocal(ITEM_TYPE_STORED.getUnlocalizedNameInefficiently(new ItemStack(ITEM_TYPE_STORED))+".name")};
+		} else if (method == 4) {
+			if (ITEM_TYPE_STORED != null)
+				return new Object[]{Item.getIdFromItem(ITEM_TYPE_STORED)};
+		} else if (method == 5) {
+			int amount = 0;
+			if (ITEM_TYPE_STORED != null)
+				amount = CURRENT_USAGE;
+			return new Object[]{amount};
+		} else if (method == 6) {
+			int[] ids = OreDictionary.getOreIDs(new ItemStack(ITEM_TYPE_STORED));
+			HashMap<Integer, String> entries = new HashMap<Integer,String>();
+			for (int i = 0; i < ids.length; i++) {
+				entries.put(i, OreDictionary.getOreName(ids[i]));
+			}
+			return new Object[]{entries};
 		}
 		return new Object[0];
 	}
