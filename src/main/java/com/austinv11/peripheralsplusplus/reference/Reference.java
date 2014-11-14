3ebf8bfaecf158adf6f894abf95c6fe6169b3c29
austinv11 <austinv11@gmail.com>
2014-11-14 07:08:44 -0500

    Fixing my grade stuff

--- src/main/java/com/austinv11/peripheralsplusplus/reference/Reference.java
+++ src/main/java/com/austinv11/peripheralsplusplus/reference/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 	public static final String SERVER_PROXY_CLASS = "com.austinv11.peripheralsplusplus.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "com.austinv11.peripheralsplusplus.proxy.ClientProxy";
 	public static final String GUI_FACTORY_CLASS = "com.austinv11.peripheralsplusplus.client.gui.GUIFactory";
-	public static final int CHAT_BOX_UPGRADE = 101; //TODO register the ids on the CC wiki
+	public static final int CHAT_BOX_UPGRADE = 101;
 	public static final int PLAYER_SENSOR_UPGRADE = 102;
 	public static final int COMPASS_UPGRADE = 103;
 	public static final int XP_UPGRADE = 104;
