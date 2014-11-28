0fbf567adf346c49d028b0b64f1a6968f01400e1
austinv11 <austinv11@gmail.com>
2014-11-28 13:31:37 -0500

    Aren't inside jokes wonderful? ;)

--- src/main/java/com/austinv11/peripheralsplusplus/init/Recipes.java
+++ src/main/java/com/austinv11/peripheralsplusplus/init/Recipes.java
@@ -8,11 +8,19 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class Recipes {
 
 	public static void init(){
+		//Hehehe inside jokes ftw!
+		ItemStack cDust = new ItemStack(Items.redstone);
+		cDust.setStackDisplayName(StatCollector.translateToLocal("item.peripheralsplusplus:dustRedstone.name"));
+		ItemStack tIngot = new ItemStack(Items.iron_ingot);
+		tIngot.setStackDisplayName(StatCollector.translateToLocal("item.peripheralsplusplus:ingotIron.name"));
+		GameRegistry.addShapelessRecipe(cDust, new ItemStack(Items.redstone));
+		GameRegistry.addShapelessRecipe(tIngot, new ItemStack(Items.iron_ingot));
 		if (Config.enableChatBox)
 			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.chatBox), "gng", "ndn", "gng", 'g', "ingotGold", 'n', new ItemStack(Blocks.noteblock), 'd', "gemDiamond"));
 		if (Config.enablePlayerSensor)
