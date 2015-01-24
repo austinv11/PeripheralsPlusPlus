90c98e0bc39fd6bbbc98fdddbdf82387dcc15bb7
austinv11 <austinv11@gmail.com>
2015-01-24 13:10:24 -0500

    The RF charger only implements IEnergyReceiver now

---
+++ src/main/java/com/austinv11/peripheralsplusplus/tiles/NetworkedTileEntity.java
@@ -0,0 +1,27 @@
+package com.austinv11.peripheralsplusplus.tiles;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+
+public abstract class NetworkedTileEntity extends TileEntity {
+
+	public NetworkedTileEntity() {
+		super();
+	}
+
+	@Override
+	public Packet getDescriptionPacket() {
+		NBTTagCompound tag = new NBTTagCompound();
+		writeToNBT(tag);
+		return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 3, tag);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+		readFromNBT(pkt.func_148857_g());
+	}
+
+}
