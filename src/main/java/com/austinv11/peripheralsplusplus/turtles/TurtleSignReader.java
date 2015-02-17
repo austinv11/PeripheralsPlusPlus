94a67198474cc5ecf22ad160c8aca29f4f1acd91
austinv11 <austinv11@gmail.com>
2015-02-17 09:03:47 -0500

    Fixing @grandmind derps

--- src/main/java/com/austinv11/peripheralsplusplus/turtles/TurtleSignReader.java
+++ src/main/java/com/austinv11/peripheralsplusplus/turtles/TurtleSignReader.java
@@ -1,7 +1,6 @@
 package com.austinv11.peripheralsplusplus.turtles;
 
 import com.austinv11.peripheralsplusplus.init.ModBlocks;
-import com.austinv11.peripheralsplusplus.init.ModItems;
 import com.austinv11.peripheralsplusplus.reference.Reference;
 import com.austinv11.peripheralsplusplus.turtles.peripherals.PeripheralSignReader;
 import cpw.mods.fml.relauncher.Side;
@@ -15,33 +14,38 @@ import net.minecraft.util.IIcon;
 public class TurtleSignReader implements ITurtleUpgrade
 {
     @Override
-    public int getUpgradeID() { return Reference.SIGN_READER_UPGRADE; }
+    public int getUpgradeID() {
+		return Reference.SIGN_READER_UPGRADE;
+	}
 
     @Override
-    public String getUnlocalisedAdjective() { return "peripheralsplusplus.turtleUpgrade.signReader"; }
+    public String getUnlocalisedAdjective() {
+		return "peripheralsplusplus.turtleUpgrade.signReader";
+	}
 
     @Override
-    public TurtleUpgradeType getType() { return TurtleUpgradeType.Peripheral; }
+    public TurtleUpgradeType getType() {
+		return TurtleUpgradeType.Peripheral;
+	}
 
     @Override
-    public ItemStack getCraftingItem() { return new ItemStack(Items.sign); }
+    public ItemStack getCraftingItem() {
+		return new ItemStack(Items.sign);
+	}
 
     @Override
-    public IPeripheral createPeripheral(ITurtleAccess turtle, TurtleSide side)
-    {
+    public IPeripheral createPeripheral(ITurtleAccess turtle, TurtleSide side) {
         return new PeripheralSignReader(turtle);
     }
 
     @Override
-    public TurtleCommandResult useTool(ITurtleAccess turtle, TurtleSide side, TurtleVerb verb, int direction)
-    {
+    public TurtleCommandResult useTool(ITurtleAccess turtle, TurtleSide side, TurtleVerb verb, int direction) {
         return null;
     }
 
     @SideOnly(Side.CLIENT)
     @Override
-    public IIcon getIcon(ITurtleAccess turtle, TurtleSide side)
-    {
+    public IIcon getIcon(ITurtleAccess turtle, TurtleSide side) {
         return ModBlocks.dummyBlock.getIcon(0,0);
     }
 
