f68110b3afc4bd7d727ed60cc22d10497b74983c
Rolando Islas <admin@rolandoislas.com>
2015-02-27 14:57:23 -0700

    Changed tabs from spaces to tab characters to fit with existing code style

--- src/main/java/com/austinv11/peripheralsplusplus/proxy/ClientProxy.java
+++ src/main/java/com/austinv11/peripheralsplusplus/proxy/ClientProxy.java
@@ -58,7 +58,7 @@ public class ClientProxy extends CommonProxy {
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerEvents() {
-        super.registerEvents();
+		super.registerEvents();
 		FMLCommonHandler.instance().bus().register(new SmartHelmetHandler());
 		MinecraftForge.EVENT_BUS.register(new SmartHelmetHandler());
 	}
