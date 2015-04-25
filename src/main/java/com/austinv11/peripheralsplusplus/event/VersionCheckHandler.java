7ec7fed509e3f046c592659e766ae0f70f95a4d3
austinv11 <austinv11@gmail.com>
2015-04-25 09:27:36 -0400

    Disabled version notifications in a dev environment

--- src/main/java/com/austinv11/peripheralsplusplus/event/VersionCheckHandler.java
+++ src/main/java/com/austinv11/peripheralsplusplus/event/VersionCheckHandler.java
@@ -1,5 +1,6 @@
 package com.austinv11.peripheralsplusplus.event;
 
+import com.austinv11.collectiveframework.minecraft.CollectiveFramework;
 import com.austinv11.peripheralsplusplus.PeripheralsPlusPlus;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
@@ -9,7 +10,7 @@ public class VersionCheckHandler {
 	
 	@SubscribeEvent
 	public void onTick(TickEvent.ClientTickEvent event) {
-		if (event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null) {
+		if (event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null && !CollectiveFramework.IS_DEV_ENVIRONMENT) {
 			if (PeripheralsPlusPlus.tries > 0 && PeripheralsPlusPlus.versionMessage != null) {
 				Minecraft.getMinecraft().thePlayer.addChatComponentMessage(PeripheralsPlusPlus.versionMessage);
 				PeripheralsPlusPlus.tries--;
