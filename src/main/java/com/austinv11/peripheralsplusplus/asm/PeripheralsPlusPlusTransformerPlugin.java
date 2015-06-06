60a5016db4c7e7f6e5f380da32de387734d60ff5
austinv11 <austinv11@gmail.com>
2015-06-06 16:05:27 -0400

    Need to do this in order to sync a fix
    This is not the commit you are looking for

---
+++ src/main/java/com/austinv11/peripheralsplusplus/asm/PeripheralsPlusPlusTransformerPlugin.java
@@ -0,0 +1,33 @@
+package com.austinv11.peripheralsplusplus.asm;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+public class PeripheralsPlusPlusTransformerPlugin implements IFMLLoadingPlugin {
+	
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[]{"com.austinv11.peripheralsplusplus.asm.Transformer"};
+	}
+	
+	@Override
+	public String getModContainerClass() {
+		return "com.austinv11.peripheralsplusplus.asm.DummyContainer";
+	}
+	
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+	
+	@Override
+	public void injectData(Map<String, Object> data) {
+		
+	}
+	
+	@Override
+	public String getAccessTransformerClass() {
+		return "com.austinv11.peripheralsplusplus.asm.PeripheralsPlusPlusAccessTransformer";
+	}
+}
--- src/main/java/com/austinv11/peripheralsplusplus/event/VersionCheckHandler.java
+++ src/main/java/com/austinv11/peripheralsplusplus/event/VersionCheckHandler.java
@@ -4,10 +4,13 @@ import com.austinv11.collectiveframework.minecraft.CollectiveFramework;
 import com.austinv11.peripheralsplusplus.PeripheralsPlusPlus;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 
 public class VersionCheckHandler {
 	
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public void onTick(TickEvent.ClientTickEvent event) {
 		if (event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null && !CollectiveFramework.IS_DEV_ENVIRONMENT) {
